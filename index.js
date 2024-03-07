c3df1b2388e7d31edb5a5a4745a5e3b958f3b4bb
anbumani12 <anbumani15012002@gmail.com>
2024-03-07 16:00:16 +0530

    task completed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 dotenv.config()
 
 const Port = process.env.PORT;
-const DatabaseAPI = process.env.DATABASE_URL || 'mongodb+srv://santhoshsrinivasan29092001:sandy2001@cluster0.f12ypnu.mongodb.net'
+const DatabaseAPI = process.env.DATABASE_URL || 'mongodb+srv://anbumani29092001:sandy2001@cluster0.f12ypnu.mongodb.net'
 
 mongoose.set('strictQuery', false)
 
